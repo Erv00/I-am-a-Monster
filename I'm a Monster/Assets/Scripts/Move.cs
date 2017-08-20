53e0305702d4a658e52f554726d7a8d316da1b88
Erv00 <pek.erv00@gmail.com>
2017-08-20 09:53:58 +0200

    Basic Movement

---
+++ I'm a Monster/Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour {
+
+	NavMeshAgent agent;
+	void Start(){
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+
+	void Update(){
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit inf;
+			if (Physics.Raycast(ray,out inf)) {
+				agent.SetDestination (inf.point);
+			}
+		}
+	}
+}
