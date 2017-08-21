4925a79b1a3408814ecde0fcf977fcc9866e6314
Erv00 <pek.erv00@gmail.com>
2017-08-21 13:26:46 +0200

    Explosion Cooldown

--- I'm a Monster/Assets/Scripts/Exploson.cs
+++ I'm a Monster/Assets/Scripts/Exploson.cs
@@ -5,8 +5,17 @@ using UnityEngine.AI;
 
 [RequireComponent(typeof(Health))]
 public class Exploson : MonoBehaviour {
+
+	public float explosonCooldownS = 2;
+	[SerializeField]
+	private int expCooldown;
+
+	void Start(){
+		expCooldown = Mathf.RoundToInt(explosonCooldownS * 50f);
+	}
+
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButtonDown(0) && expCooldown <=0) {
 			Ray mouse = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit Minf;
 			if (Physics.Raycast(mouse,out Minf)) {
@@ -15,15 +24,15 @@ public class Exploson : MonoBehaviour {
 				Physics.Raycast (pos, out posInf);
 				Debug.DrawLine (transform.position, posInf.point, Color.green, 2);
 				if (posInf.point == Minf.point) {
+					expCooldown = Mathf.RoundToInt(explosonCooldownS * 50);
 					Collider[] colliders = Physics.OverlapSphere (Minf.point, 5);
 					foreach (Collider hit in colliders) {
 						Rigidbody rb = hit.GetComponent<Rigidbody> ();
 
-						if (rb != null && rb != GetComponent<Rigidbody> ())
+						if (rb != null)
 							rb.AddExplosionForce (500, Minf.point, 5, 3.0F);
 						DoDamage (hit);
 
-						//NOT SURE
 						NavMeshAgent ag = hit.GetComponent<NavMeshAgent>();
 						if (ag != null) {
 							ag.enabled = false;
@@ -33,9 +42,10 @@ public class Exploson : MonoBehaviour {
 				}
 			}
 		}
-		if (Input.GetKeyDown (KeyCode.E)) {
+		if (Input.GetKeyDown (KeyCode.E) && expCooldown <= 0) {
 			GetComponent<Health> ().TakeDamage (2, gameObject, Enums.DamageTypes.EXPLOSION);
 			Collider[] colliders = Physics.OverlapSphere(transform.position, 5);
+			expCooldown = Mathf.RoundToInt(explosonCooldownS * 50);
 			foreach (Collider hit in colliders)
 			{
 				Rigidbody rb = hit.GetComponent<Rigidbody>();
@@ -43,6 +53,12 @@ public class Exploson : MonoBehaviour {
 				if (rb != null && rb != GetComponent<Rigidbody>())
 					rb.AddExplosionForce(500, transform.position, 5, 3.0F);
 				DoDamage (hit);
+
+				NavMeshAgent ag = hit.GetComponent<NavMeshAgent>();
+				if (ag != null) {
+					ag.enabled = false;
+					StartCoroutine (Concussion (ag));
+				}
 			}
 		}
 	}
@@ -59,5 +75,10 @@ public class Exploson : MonoBehaviour {
 		a.enabled = true;
 		a.gameObject.GetComponent<Rigidbody> ().isKinematic = true;
 		a.gameObject.GetComponent<Rigidbody> ().isKinematic = false;
+	}
+
+	void FixedUpdate(){
+		if(expCooldown > 0)
+			expCooldown--;
 	}
 }
